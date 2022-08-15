55fa7a71e2f87fcd80bbe0ee4d751bd8fa8808e8
abb128 <65567823+abb128@users.noreply.github.com>
2022-08-15 16:35:37 +0300

    Add comments to XR.h

--- src/XR.h
+++ src/XR.h
@@ -10,30 +10,42 @@
 struct IGameComponent;
 extern struct IGameComponent XR_Component;
 
-
+/* Opaque internal struct */
 struct XRFrameContext;
 
+/* Data to be used by the renderer to render a single view. */
 struct XRViewRender {
-    struct Matrix pose;       // view matrix
-    struct Matrix projection; // projection matrix
-    GfxResourceID framebuffer;
+    struct Matrix pose;
+    struct Matrix projection;
+    GfxResourceID framebuffer
 };
 
 
-/* Whether or not the XR rendering methods should be called. */
+/* Whether or not XR is active. If false, don't call the frame functions */
 cc_bool XR_IsActive(void);
 
-
+/* Initializes a frame context, should be done at the beginning of a frame. */
+/* Don't forget to free the frame context via XR_FreeFrameContext */
 struct XRFrameContext *XR_InitFrameContext();
+
+/* Frees a frame context, should be called at the end of a frame */
 void XR_FreeFrameContext(struct XRFrameContext *ctx);
 
+/* Waits for the next frame */
 void XR_WaitFrame(struct XRFrameContext *ctx);
 
+/* Readies and begins frame rendering. Should be called immediately before */
+/* rendering is started. Next create an empty XR_RenderNextView and call */
+/* XR_RenderNextView in a loop until it returns false. */
 void XR_BeginFrame(struct XRFrameContext *ctx);
 
-/* Iterate this in a loop until it returns false */
+/* Returns false if no more views are available. This should be called in */
+/* a loop after XR_BeginFrame, and the loop should be broken once false. */
+/* If true, XRViewRender has been filled out and the renderer should render */
+/* to the given framebuffer using the given pose and projection. */
 cc_bool XR_RenderNextView(struct XRFrameContext *ctx, struct XRViewRender *view);
 
+/* Submits frame, call after done rendering, before XR_FreeFrameContext */
 void XR_SubmitFrame(struct XRFrameContext *ctx);
 
 
